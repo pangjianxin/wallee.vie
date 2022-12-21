44cd068b9521049c01a8e89ff9af1a68999bbb48
pangjianxin <343516704@qq.com>
2022-12-21 14:25:31 +0800

    add some styles for card-template generating

--- wallee.vie/components.d.ts
+++ wallee.vie/components.d.ts
@@ -16,6 +16,7 @@ declare module '@vue/runtime-core' {
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
+    ElCol: typeof import('element-plus/es')['ElCol']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElEmpty: typeof import('element-plus/es')['ElEmpty']
--- wallee.vie/src/views/index.vue
+++ wallee.vie/src/views/index.vue
@@ -1,6 +1,71 @@
 <template>
-  <div>
-    <el-button icon="User" @click="get"></el-button>
+  <div class="small-panel-box">
+    <el-row :gutter="20">
+      <el-col :sm="12" :lg="6">
+        <div class="small-panel user-reg suspension">
+          <div class="small-panel-title">
+            {{ "dashboard.Member registration" }}
+          </div>
+          <div class="small-panel-content">
+            <div class="content-left">
+              <el-icon size="20" color="#F56C6C">
+                <user></user>
+              </el-icon>
+              <span id="user_reg_number">5456</span>
+            </div>
+            <div class="content-right">+14%</div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :sm="12" :lg="6">
+        <div class="small-panel file suspension">
+          <div class="small-panel-title">
+            {{ "dashboard.Number of attachments Uploaded" }}
+          </div>
+          <div class="small-panel-content">
+            <div class="content-left">
+              <el-icon size="20" color="#F56C6C">
+                <user></user>
+              </el-icon>
+              <span id="file_number">1234</span>
+            </div>
+            <div class="content-right">+50%</div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :sm="12" :lg="6">
+        <div class="small-panel users suspension">
+          <div class="small-panel-title">
+            {{ "dashboard.Total number of members" }}
+          </div>
+          <div class="small-panel-content">
+            <div class="content-left">
+              <el-icon size="20" color="#F56C6C">
+                <user></user>
+              </el-icon>
+              <span id="users_number">9486</span>
+            </div>
+            <div class="content-right">+28%</div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :sm="12" :lg="6">
+        <div class="small-panel addons suspension">
+          <div class="small-panel-title">
+            {{ "dashboard.Number of installed plug-ins" }}
+          </div>
+          <div class="small-panel-content">
+            <div class="content-left">
+              <el-icon size="20" color="#F56C6C">
+                <user></user>
+              </el-icon>
+              <span id="addons_number">875</span>
+            </div>
+            <div class="content-right">+88%</div>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -8,12 +73,6 @@
 import useOidcStore from "/@/store/modules/useOidcStore";
 import useAppConfig from "/@/store/modules/useApplicationConfigStore";
 const appconfig = useAppConfig();
-async function get() {
-  console.log("hel");
-
-  await appconfig.initConfig();
-  console.log(appconfig.getCurrentUser);
-}
 </script>
 <route>
     {
